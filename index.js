53a53fea3d122ae972fd4b8c6df1782ed9033da3
Clauber Stipkovic <clauber.halic@gmail.com>
2013-08-21 11:13:34 -0300

    JsHint at index.js

--- index.js
+++ index.js
@@ -1,38 +1,46 @@
+/*jshint forin:true, noarg:true, noempty:true, eqeqeq:true, bitwise:true, strict:true, undef:true, 
+unused:true, curly:true, browser:true, node:true, indent:4, maxerr:50 */
+
+/*global require*/
+
 /*
 * Wcompilr - Watches and compiles coffescript, sass and less
 * author: Paulo Martins <@m4rtinsp>
 * version: 0.0.5
 */
-var fs 		= require('fs'),
-	watch 	= require('node-watch'),
-	compile = require('./lib/compile.js'),
-	build 	= require('./lib/build.js');
+var fs = require("fs"),
+	watch = require("node-watch"),
+	compile = require("./lib/compile.js"),
+	build = require("./lib/build.js"),
+    watchFiles,
+    buildFiles,
+    checkFiles;
 
 exports.init = function(settings) {
-	'use strict';
+	"use strict";
 
 	// Start
-	console.log('Wcompilr Start');
+	console.log("Wcompilr Start");
 	
-	var settings = require('./lib/settings.js').init(settings);
+	var initSettings = require("./lib/settings.js").init(settings);
 
 	switch(process.argv[2]) {
-		case 'watch':
-			watchFiles(settings);
-			break;
-		case 'build':
-			buildFiles(settings);
-			break;
-		default:
-			console.log('Invalid command: Choose build or watch');
+	case "watch":
+		watchFiles(initSettings);
+		break;
+	case "build":
+		buildFiles(initSettings);
+		break;
+	default:
+		console.log("Invalid command: Choose build or watch");
 	}
-}
+};
 
 watchFiles = function(settings) {
-	'use strict';
+	"use strict";
 
 	if (settings.compile !== undefined) {
-		console.log('Watching...\r\n');
+		console.log("Watching...\r\n");
 		
 		var files = checkFiles(settings.compile, settings);
 
@@ -42,32 +50,33 @@ watchFiles = function(settings) {
 			});
 		}
 	}
-}
+};
 
 buildFiles = function(settings) {
-	'use strict';
+	"use strict";
 
 	if (settings.build !== undefined) {
-		console.log('Building...\r\n');
+		console.log("Building...\r\n");
 		build.files(settings);
 	}
-}
+};
 
 checkFiles = function(list, settings) {
+    "use strict";
 	var files = [];
 
 	for (var preprocessor in list) {
 		for (var file in list[preprocessor]) {
 			var item = list[preprocessor][file];
-			var type = item.split('.')[1];
+			var type = item.split(".")[1];
 
 			if (fs.existsSync(settings.pathByType(type) + item)) {
 				files.push(settings.pathByType(type) + item);
 			} else {
-				console.log('File not found: ' + settings.pathByType(type) + item);
+				console.log("File not found: " + settings.pathByType(type) + item);
 			}
 		}
 	}
 
 	return files;
-}
+};
